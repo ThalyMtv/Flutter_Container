cb4ed35e8180beb8b018ad43b1f37153e3286762
Talita Mantovani <thalitymtv@gmail.com>
2023-08-13 12:37:13 -0300

    Container_class

---
+++ mi_card_flutter_new/android/app/src/main/kotlin/com/example/mi_card_flutter_new/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mi_card_flutter_new
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mi_card_flutter_new/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp(),    
+  );
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 16, 200, 93),
+        title: const Center(child: Text(
+          "Segurança do Trabalho", textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Color.fromARGB(255, 93, 92, 91)
+            ),
+          ),
+        ),
+      ), 
+      body: SafeArea(
+        child: Container(
+          height: 100.0,
+          width: 100.0,
+          margin: const EdgeInsets.only(left: 50.0),
+          padding: const EdgeInsetsDirectional.fromSTEB(8.0, 40.0, 20.0, 8.0),
+          color: Colors.white,
+          child: const Text("Container 1"),
+        ),
+      ),
+      ),
+    );
+  }  
+}
+
